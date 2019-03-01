7baa668f2e55604ae0c13f0ee8fc7ca5df89ce1b
owalid <othmane.el.ayadi@gmail.com>
2019-03-01 15:03:56 +0100

    script yml

--- script/yarn_npm.sh
+++ script/yarn_npm.sh
@@ -1,5 +1,5 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install nodejs
-sudo apt install yarn
-sudo apt install --no-install-recommends yarn
+sudo apt-get install -y nodejs
+sudo apt-get install -y yarn
+sudo apt install -y --no-install-recommends yarn
