4e41025d841163ecc8094de90fc7c25922cce71e
owalid <othmane.el.ayadi@gmail.com>
2019-03-01 16:27:38 +0100

    done

--- script/yarn_npm.sh
+++ script/yarn_npm.sh
@@ -1,5 +1,7 @@
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install -y nodejs
-sudo apt-get install -y yarn
-sudo apt install -y --no-install-recommends yarn
+apt-get update -qq \ && apt-get install -y --no-install-recommends build-essential \ apt-transport-https curl ca-certificates gnupg2 apt-utils nodejs 
+# Install node from nodesource  
+curl -sL https://deb.nodesource.com/setup_10.x | bash - \ && apt-get install -y nodejs 
+# Install yarn 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \ && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \ && apt-get update -qq \ && apt-get install -y yarn 
+# Test RUN 
+yarn --version
