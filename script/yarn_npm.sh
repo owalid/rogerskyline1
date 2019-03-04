76a62aa7d13ab05fb84680463d347cf45c86e232
owalid <othmane.el.ayadi@gmail.com>
2019-03-04 16:28:29 +0100

    update script yarn npm

--- script/yarn_npm.sh
+++ script/yarn_npm.sh
@@ -1,4 +1,9 @@
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-apt-get install -y nodejs
-apt-get install -y yarn
-apt-get update
+#install npm
+apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
+apt-get install nodejs
+
+#install yarn 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install yarn
